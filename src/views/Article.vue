548f82560c7d91b750806a33b9a192c2da952f2b
g-traub <guillaume.trb@hotmail.com>
2020-05-18 17:08:43 +0200

    created an json file for articles + beginning of article logic

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,9 +1,31 @@
-<template>
-  <div></div>
+<template lang="pug">
+  div Article
 </template>
 
 <script>
+import axios from 'axios'
+
+// Not actually an API url, but the path to our local json file
+const API_URL = 'data/articles.json'
+
 export default {
-  name: 'Article'
+  name: 'Article',
+  data() {
+    return {
+      articles: null
+    }
+  },
+  computed: {
+    article() {
+      return this.articles.find(article => article.name === this.$route.params.articleName)
+    }
+  },
+  async created() {
+    axios(API_URL)
+      .then(res => {
+        this.articles = res.data
+      })
+      .catch(err => console.error(err))
+  }
 }
 </script>
