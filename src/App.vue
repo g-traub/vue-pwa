b678bb17ad73de2232c04a46b81e3c4665be9788
g-traub <guillaume.trb@hotmail.com>
2020-05-18 22:41:22 +0200

    added fonts and reset css

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,12 @@
     <router-view />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Josefin+Sans:wght@300;400&family=Oswald:wght@400;500&display=swap');
+
+html,
+body {
+  font-family: 'Oswald', sans-serif;
+}
+</style>
