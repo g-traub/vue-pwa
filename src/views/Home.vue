0052b1652b35cdc1c38dcc577aaf8d01efe6ba25
g-traub <guillaume.trb@hotmail.com>
2020-05-19 15:04:40 +0200

    section insta homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,13 @@ div
         button-link.articles__button(:to="{name: 'ArticlesList'}" variant="primary") VOIR D'AUTRES ARTICLES
   
   section.home__insta
-    h2.insta__title VIENS DÉCOUVRIR MES DERNIERS POSTS INSTAGRAM
+    h2.insta__title VIENS DÉCOUVRIR MES DERNIERS POSTS INSTAGRAM !
+    .insta__images
+      img.insta__image(src="@/assets/img/insta0.png")
+      img.insta__image(src="@/assets/img/insta1.png")
+      img.insta__image(src="@/assets/img/insta2.png")
+      img.insta__image(src="@/assets/img/insta3.png")
+      img.insta__image(src="@/assets/img/insta4.png")
 
   section.home__about
     p Salut ! Moi c’est 
@@ -44,13 +50,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/colors';
+
 .articles__title,
 .insta__title {
   font-weight: 500;
   font-size: 2.1rem;
 }
 .articles__content {
-  padding: calc(5% + 1rem);
+  padding: 4rem calc(5% + 1rem);
 }
 .articles__cards {
   margin: 3rem auto 0 auto;
@@ -70,5 +78,26 @@ export default {
   background-repeat: no-repeat;
   background-position: top left;
   background-size: contain;
+}
+.home__insta {
+  background-color: $primaryGreen;
+  padding-bottom: 3rem;
+}
+.insta__title {
+  padding: 3rem calc(5% + 1rem);
+  color: $white;
+}
+.insta__images {
+  display: flex;
+  justify-content: space-between;
+}
+.insta__image {
+  margin-top: 1.5rem;
+  transition: all cubic-bezier(0.23, 1, 0.32, 1) 0.3s;
+  box-shadow: 4px 8px 16px rgba(0, 0, 0, 0.1);
+  &:hover {
+    transform: scale(1.025);
+    box-shadow: 4px 8px 20px rgba(0, 0, 0, 0.1);
+  }
 }
 </style>
