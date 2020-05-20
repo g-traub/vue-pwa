e749ac0112b94815d4eb775678f733b5ed1b4199
g-traub <guillaume.trb@hotmail.com>
2020-05-20 08:34:40 +0200

    icon paths

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,18 @@ module.exports = {
   pwa: {
     name: 'Stranger Plants',
     themeColor: '#36f0a2',
-    msTileColor: '#333333',
+    msTileColor: '#ffffff',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     manifestOptions: {
       display: 'fullscreen'
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png'
     }
   }
 }
